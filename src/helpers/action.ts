e02ed759466c1af0570eea97506a9dbce6ef7915
dcapslock <dcapes-davis@outlook.com.au>
2026-02-10 14:29:15 +1100

    feat: Add button option for state display.

--- src/helpers/action.ts
+++ src/helpers/action.ts
@@ -1,3 +1,5 @@
+import { hass, hass_base_el } from "./hass";
+
 export function bindActionHandler(element, options = {}) {
   customElements.whenDefined("long-press").then(() => {
     const longpress = document.body.querySelector("long-press") as any;
@@ -8,4 +10,13 @@ export function bindActionHandler(element, options = {}) {
     actionHandler.bind?.(element, options);
   });
   return element;
+}
+
+export async function hassAction(actionConfig, entityId = "") {
+  const hsEl = await hass_base_el();
+  let actionJSON = JSON.stringify(actionConfig);
+  actionJSON = actionJSON.replace("config.entity", entityId);
+  const action = JSON.parse(actionJSON);
+  const detail = { config: { tap_action: action }, action: "tap" };
+  hsEl.dispatchEvent(new CustomEvent("hass-action", { detail: detail, bubbles: true, composed: true }));
 }
